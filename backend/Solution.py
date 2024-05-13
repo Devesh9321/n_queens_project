e5c49df24e1e313044b04432f281ad5dd42351a7
“Devesh9321” <deveshpatilamt@gmail.com>
2024-05-14 01:11:55 +0530

    complete

--- backend/Solution.py
+++ backend/Solution.py
@@ -1,47 +1,22 @@
-# Python module for solving N Queen's Problem
-
 class Solution:
-    def isSafe(self, row, col, board, n):
-        # Check upper-left diagonal
-        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-            if board[i][j] == 'Q':
+    def isSafe(self, row, col, queens):
+        for r, c in queens:
+            if row == r or col == c or abs(row - r) == abs(col - c):
                 return False
-        
-        # Check left
-        for j in range(col, -1, -1):
-            if board[row][j] == 'Q':
-                return False
-        
-        # Check lower-left diagonal
-        for i, j in zip(range(row, n), range(col, -1, -1)):
-            if board[i][j] == 'Q':
-                return False
-        
         return True
 
-    def solve(self, col, board, ans, n):
+    def solve(self, col, queens, ans, n):
         if col == n:
-            ans.append(list(board))
+            ans.append(queens[:])
             return
 
         for row in range(n):
-            if self.isSafe(row, col, board, n):
-                board[row] = board[row][:col] + 'Q' + board[row][col+1:]
-                self.solve(col + 1, board, ans, n)
-                board[row] = board[row][:col] + '.' + board[row][col+1:]
+            if self.isSafe(row, col, queens):
+                queens.append((row, col))
+                self.solve(col + 1, queens, ans, n)
+                queens.pop()
 
     def solveNQueens(self, n):
         ans = []
-        board = ['.' * n for _ in range(n)]
-        self.solve(0, board, ans, n)
+        self.solve(0, [], ans, n)
         return ans
-
-# n = 4
-# obj = Solution()
-# ans = obj.solveNQueens(n)
-
-# for idx, solution in enumerate(ans):
-#     print(f"Arrangement {idx+1}:")
-#     for row in solution:
-#         print(row)
-#     print()
--- backend/__init__.py
+++ backend/__init__.py
@@ -1,16 +1,35 @@
-from flask import Flask,render_template,jsonify
+from flask import (
+    Flask, render_template, jsonify, redirect, render_template, request, session, url_for
+)
 from . import Solution
 
 app = Flask(__name__)
+obj = Solution.Solution()
 
-@app.route('/')
+
+@app.route('/',  methods=('GET', 'POST'))
 def index():
-    n = 4  # Board size
-    obj = Solution.Solution()
-    solutions = obj.solveNQueens(n)
-    return render_template('index.html', solutions=jsonify(solutions))
+    solutions = {'Data': 'none'}
+    if request.method == 'POST':
+        # print(request.form['inputQueen'])
+        Nqueen = request.form['inputQueen']
+        error = None
+        if not Nqueen:
+            error = 'N is required.'
 
-# if __name__ == '__main__':
-    # app.run(debug=True)
+        if error is None:
+            obj = Solution.Solution()
+            solutions = obj.solveNQueens(int(Nqueen))
+            return render_template('index.html', solutions=solutions)
 
+    return render_template('index.html', solutions=solutions)
+
+
+@app.route('/solutions', methods=['POST'])
+def get_solutions():
+    data = request.get_json()
+    Nqueen = data['inputQueen']
+    # Nqueen = 5
+    solutions = obj.solveNQueens(int(Nqueen))
+    return jsonify({'solutions': solutions})
 
